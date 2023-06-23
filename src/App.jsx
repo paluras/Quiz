f8a0f867d1356cf0ecf079eeb2e9816daceaa2a6
Palura Andrei <98059798+paluras@users.noreply.github.com>
2023-06-23 14:08:10 +0300

    Add Order Function

--- src/App.jsx
+++ src/App.jsx
@@ -9,29 +9,40 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-
-
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(1);
   const [ans, setAns] = useState([]);
   const [truty, setTruty] = useState("");
   const [materie, setMaterie] = useState(true);
   const [haide, setHaide] = useState(quiz);
-  const[corect, setCorect] = useState("")
+  const [corect, setCorect] = useState("");
+  const [ordine, setOrdine] = useState(true)
+  const [textOrdine, setTextOrdine] = useState("Vreau in ordine")
 
+  const handleOrdine = () => {
+     setOrdine(!ordine)
+     if(ordine == true){
+      
+      setTextOrdine("Vreau amestecate")
+    }else {
+    
+    setTextOrdine("Vreau in ordine")
+  } 
+  }
+
+  console.log(ordine);
 
   const findTrueValue = () => {
-   
-    const trueOption =  haide.questions[count].options.find(option => option.bool === "true");
+    const trueOption = haide.questions[count].options.find(
+      (option) => option.bool === "true"
+    );
     if (trueOption) {
-      return trueOption.id +")" + trueOption.text;
+      return trueOption.id + ")" + trueOption.text;
     }
     return "No true value found.";
   };
 
-
   console.log(findTrueValue());
-
 
   const handleMaterie = () => {
     materie ? setMaterie(false) : setMaterie(true);
@@ -41,34 +52,51 @@ function App() {
     } else setHaide(quiz);
   };
 
-  
-
   const handleClick = (event) => {
     let buttonValue = event.target.value;
-    let materie =  event.target.getAttribute("materie"); 
+    let materie = event.target.getAttribute("materie");
     let buttonTrue = event.target.getAttribute("bool");
-    let question = haide.questions[count].question
-    setTruty(buttonTrue)
-   
-  
+    let question = haide.questions[count].question;
+    setTruty(buttonTrue);
 
     setAns((prevState) => [
       ...prevState,
-     question +"\n"+"Raspuns Corect:"+ findTrueValue()+"\n" + materie+ "-" + count + "-" + buttonValue + " - " + buttonTrue ,
+      question +
+        "\n" +
+        "Raspuns Corect:" +
+        findTrueValue() +
+        "\n" +
+        materie +
+        "-" +
+        count +
+        "-" +
+        buttonValue +
+        " - " +
+        buttonTrue,
     ]);
     setCount("");
+    if(ordine == false){
+      setCount(count+1)
+     
+    }else {
     setCount(getRandomInt(150));
-    if (buttonTrue === "false"){
+  
+  } 
+   
+    if (buttonTrue === "false") {
       setCorect(haide.questions[count]);
-     }
+    }
   };
- 
 
- 
   return (
     <>
       <h3 className="container-title">{haide.questions[count].question}</h3>
-      <button className="change" onClick={handleMaterie}>Schimba Materia</button>
+      <button className="change" onClick={handleMaterie}>
+        Schimba Materia
+      </button>
+      <button className="change" onClick={handleOrdine}>
+        {textOrdine}
+      </button>
       <div></div>
       <div className="answers-multipole">
         <div className="answer">
@@ -77,7 +105,7 @@ function App() {
             onClick={handleClick}
             value="A"
             bool={haide.questions[count].options[0].bool}
-            materie = {haide.questions[count].options[0].materie}
+            materie={haide.questions[count].options[0].materie}
           >
             A
           </button>
@@ -88,7 +116,7 @@ function App() {
             onClick={handleClick}
             value="B"
             bool={haide.questions[count].options[1].bool}
-            materie = {haide.questions[count].options[0].materie}
+            materie={haide.questions[count].options[0].materie}
           >
             B
           </button>
@@ -99,7 +127,7 @@ function App() {
             onClick={handleClick}
             value="C"
             bool={haide.questions[count].options[2].bool}
-            materie = {haide.questions[count].options[0].materie}
+            materie={haide.questions[count].options[0].materie}
           >
             C
           </button>
@@ -110,7 +138,7 @@ function App() {
             onClick={handleClick}
             value="D"
             bool={haide.questions[count].options[3].bool}
-            materie = {haide.questions[count].options[0].materie}
+            materie={haide.questions[count].options[0].materie}
           >
             D
           </button>
@@ -121,7 +149,7 @@ function App() {
             onClick={handleClick}
             value="E"
             bool={haide.questions[count].options[4].bool}
-            materie = {haide.questions[count].options[0].materie}
+            materie={haide.questions[count].options[0].materie}
           >
             E
           </button>
@@ -130,7 +158,9 @@ function App() {
       </div>
       <div className="the-ans">
         {ans.reverse().map((element, index) => (
-          <div className={`${element.slice(-2)}`} key={index}>{element}</div>
+          <div className={`${element.slice(-2)}`} key={index}>
+            {element}
+          </div>
         ))}
       </div>
     </>
